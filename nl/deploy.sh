46b78ce7382a770fc49d371d6a068d67fff3e905
Joep Meindertsma <joep@ontola.io>
2021-08-26 16:54:34 +0200

    Updates

--- nl/deploy.sh
+++ nl/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 bundle exec jekyll build
 # For some reason this file is skipped. This is required for hosting on the ontola.io domain using github pages.
 cp CNAME ./_site/CNAME
