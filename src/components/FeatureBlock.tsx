72c61ec9504d97b60b749d34b2a733b4785289f3
Joep Meindertsma <joep@ontola.io>
2021-12-07 13:45:14 +0100

    Add process

--- src/components/FeatureBlock.tsx
+++ src/components/FeatureBlock.tsx
@@ -1,6 +1,7 @@
 import { styled } from '@stitches/react';
 import Image from 'next/image';
 
+import { theme } from '../../stitches.config';
 import { Tool } from './Tool';
 
 interface FeatureBlockProps {
@@ -9,9 +10,9 @@ interface FeatureBlockProps {
   /** Path to image, starting from /assets/images/ */
   image: string;
   /** List of ids of technologies used (e.g. typescript) */
-  tools: string[];
-  /** Renders the image on the left side */
-  inverted?: boolean;
+  tools?: string[];
+  /** A big colored number on the side */
+  number?: number;
 }
 
 const TextContent = styled('div', {
@@ -22,6 +23,19 @@ const TextContent = styled('div', {
   marginBottom: '5rem',
 });
 
+const ImageWrapper = styled('div', {
+  gridArea: 'image',
+  width: '20em',
+  height: '15em',
+  position: 'relative',
+  top: '2rem',
+  transform: 'rotate(5deg)',
+  borderRadius: '0.5rem',
+  overflow: 'hidden',
+  marginBottom: '7rem',
+  boxShadow: theme.shadows.button,
+});
+
 const FeatureBlockWrapper = styled('div', {
   color: '$text1',
   display: 'grid',
@@ -29,6 +43,19 @@ const FeatureBlockWrapper = styled('div', {
   flex: 1,
   marginBottom: '8rem',
   gridTemplateAreas: "'text' 'image'",
+
+  '&:nth-child(odd)': {
+    [`& ${ImageWrapper}`]: {
+      transform: 'rotate(-5deg)',
+    },
+    '@media (min-width: 500px)': {
+      gridTemplateAreas: "'image text'",
+      [`& ${TextContent}`]: {
+        marginRight: '0rem',
+        marginLeft: '2rem',
+      },
+    },
+  },
 
   '@media (min-width: 500px)': {
     [`& ${TextContent}`]: {
@@ -43,44 +70,26 @@ const FeatureBlockWrapper = styled('div', {
     marginLeft: '-5rem',
     marginRight: '-5rem',
   },
-
-  variants: {
-    inverted: {
-      true: {
-        '@media (min-width: 500px)': {
-          gridTemplateAreas: "'image text'",
-          [`& ${TextContent}`]: {
-            marginRight: '0rem',
-            marginLeft: '2rem',
-          },
-        },
-      },
-    },
-  },
-});
-
-const ImageWrapper = styled('div', {
-  gridArea: 'image',
-  // margin: '0 2rem',
-  width: '20em',
-  position: 'relative',
-  top: '2rem',
-  transform: 'rotate(5deg)',
-  variants: {
-    inverted: {
-      true: {
-        transform: 'rotate(-5deg)',
-      },
-    },
-  },
 });
 
 const FeatureHeader = styled('h2', {
   fontSize: '2rem',
+  position: 'relative',
 });
 
 const ToolsWrapper = styled('div', {
   display: 'flex',
+});
+
+const BigNumber = styled('span', {
+  fontSize: '6rem',
+  position: 'absolute',
+  left: '-5rem',
+  top: '-4.5rem',
+  background: theme.colors.gradient,
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+  zIndex: 1,
 });
 
 export function FeatureBlock({
@@ -88,26 +97,31 @@ export function FeatureBlock({
   description,
   image,
   tools,
-  inverted,
+  number,
 }: FeatureBlockProps) {
   return (
-    <FeatureBlockWrapper inverted={inverted}>
+    <FeatureBlockWrapper>
       <TextContent>
-        <FeatureHeader>{title}</FeatureHeader>
+        <FeatureHeader>
+          {number && <BigNumber>{number}</BigNumber>}
+          {title}
+        </FeatureHeader>
         <p>{description}</p>
-        <ToolsWrapper>
-          {tools.map((t) => (
-            <Tool key={t} technology={t} />
-          ))}
-        </ToolsWrapper>
+        {tools && (
+          <ToolsWrapper>
+            {tools.map((t) => (
+              <Tool key={t} technology={t} />
+            ))}
+          </ToolsWrapper>
+        )}
       </TextContent>
-      <ImageWrapper inverted={inverted}>
+      <ImageWrapper>
         <Image
           width={400}
           height={300}
           src={`/assets/images/${image}`}
           alt={image}
-          objectFit="contain"
+          objectFit="cover"
         />
       </ImageWrapper>
     </FeatureBlockWrapper>
