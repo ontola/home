20aa36f302252315ed809901c68d4bec0ec5f1f0
Joep Meindertsma <joep@ontola.io>
2022-02-18 11:20:32 +0100

    Add abstract image argu, improve BigImage

--- src/components/BigImage.tsx
+++ src/components/BigImage.tsx
@@ -5,22 +5,34 @@ interface BigImageProps {
   alt: string;
 }
 
-const ImageWrapper = styled('img', {
-  maxWidth: '98vw',
+const Wrap = styled('div', {
+  margin: '3rem 0',
+  width: '98vw',
   marginLeft: 'calc(-49vw + 19rem)',
+});
+
+const ImageWrapper = styled('img', {
+  maxWidth: '50rem',
+  display: 'block',
+  margin: 'auto',
+  '@media (min-width: 1200px)': {
+    maxWidth: '1200px',
+  },
 });
 
 export function BigImage({ image, alt }: BigImageProps) {
   return (
-    <ImageWrapper
-      src={`/images/${image}`}
-      // objectFit="contain"
-      // This is not a great solution, but I see not other option.
-      // height={'100'}
-      // width={'200'}
-      // layout="fill"
-      // loading="lazy"
-      alt={alt}
-    />
+    <Wrap>
+      <ImageWrapper
+        src={`/images/${image}`}
+        // objectFit="contain"
+        // This is not a great solution, but I see not other option.
+        // height={'100'}
+        // width={'200'}
+        // layout="fill"
+        // loading="lazy"
+        alt={alt}
+      />
+    </Wrap>
   );
 }
