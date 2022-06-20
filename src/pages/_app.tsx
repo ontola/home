912a0d938304b035eccb10a104e98f8806b85729
Joep Meindertsma <joep@ontola.io>
2022-06-20 11:27:28 +0200

    Add google tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { init } from '@socialgouv/matomo-next';
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { reset } from 'stitches-reset';
 
 import { globalCss } from '../../stitches.config';
@@ -20,7 +21,24 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-940871433');
+            `,
+        }}
+      />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-940871433"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default appWithTranslation(MyApp);
