8905bcb71896df80dcaae87e83bc9f64b2133e33
Joep Meindertsma <joep@ontola.io>
2022-12-20 13:45:02 +0100

    Disable some config things #45

--- next.config.js
+++ next.config.js
@@ -7,12 +7,7 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = withBundleAnalyzer({
   i18n,
-  reloadOnPrerender: true,
   poweredByHeader: false,
   trailingSlash: true,
   reactStrictMode: true,
-  // Fixes a problem with building on AWS Amplify
-  // https://answers.netlify.com/t/build-with-next-i18next-error-error-cannot-find-module-next-i18next-config-js/34132/4
-  // https://github.com/aws-amplify/amplify-console/issues/2427
-  target: 'experimental-serverless-trace',
 });
